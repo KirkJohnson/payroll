53b90a62e2be95581906f0b54bf96204fa59724f
Kirk Johnson <kirkj@hawaii.edu>
2018-08-04 08:24:26 -1000

    styling change and lint

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -26,96 +26,96 @@ class ReportController extends Controller
     /*
      * index display all reports
      */
-    public function index(){
-      
+    public function index()
+    {
         return view('report.reports', array( 'reports' => App\Report::all()));
     }
     
     /*
      * upload report form
      */
-    public function upload (Request $request){
+    public function upload(Request $request)
+    {
       
       //make sure file is set
-      if( $request->hasFile('report') ) {
+        if ($request->hasFile('report')) {
         
         //store file get path
-        $file = $request->file('report');
+            $file = $request->file('report');
         
-        //get report id
-        $report_id = $this->get_report_number_from_file($file);
+            //get report id
+            $report_id = $this->get_report_number_from_file($file);
         
-        //check if report is alread stored
-        if( $report_id !== FALSE && is_null(App\Report::find($report_id))) {
+            //check if report is alread stored
+            if ($report_id !== false && is_null(App\Report::find($report_id))) {
           
             //if not process file and store data
-            $this->process_report_file($file, $report_id);
-        } else {
-          //redirect to home with error message
-          return redirect('/')->with('warning','Report has already been uploaded');
+                $this->process_report_file($file, $report_id);
+            } else {
+                //redirect to home with error message
+                return redirect('/')->with('warning', 'Report has already been uploaded');
+            }
+        
+        
+            //all was successful redirect to report view
+            return redirect('/view_report/'.$report_id);
         }
-        
-        
-        //all was successful redirect to report view
-        return redirect('/view_report/'.$report_id);
-      }
-      return redirect('/')->with("warning","File was not uploaded.  Please upload a file");
-      
+        return redirect('/')->with("warning", "File was not uploaded.  Please upload a file");
     }
     /*
      * view specific report
      * @input $request
      * @input $id of report to view
-     * 
+     *
      */
-    public function view( $id) {
+    public function view($id)
+    {
         
         //grab hourse worked by report_id
-        $hours = App\Hour::where("report_id", '=', $id)->orderBy('date','asc')->get();
-        if($hours->count() > 0 ){
+        $hours = App\Hour::where("report_id", '=', $id)->orderBy('date', 'asc')->get();
+        if ($hours->count() > 0) {
         
           //init array which will hold all data formatted for table
-          $data = [ "report_id" => $id ];
+            $data = [ "report_id" => $id ];
           
-          //put data into an  object
-          $hours_worked = [];
-          foreach($hours as $hour ) {
-              //orgainze data like this
-              /*
-               * [ [strtotime] =>[
-               *                        "emp_id" => hours_worked
-               *                    ]
-               * ]
-               */
-              //figure out if the lower part of pay period is the 1st or the last day
-              $h_date = strtotime($hour->date);
+            //put data into an  object
+            $hours_worked = [];
+            foreach ($hours as $hour) {
+                //orgainze data like this
+                /*
+                 * [ [strtotime] =>[
+                 *                        "emp_id" => hours_worked
+                 *                    ]
+                 * ]
+                 */
+                //figure out if the lower part of pay period is the 1st or the last day
+                $h_date = strtotime($hour->date);
               
-              //get the time for the index of array
-              $lower_time = ( date('j',$h_date) > 15 )  ? strtotime(date('m',$h_date)."/16/".date('Y', $h_date)) :
-                                $lower_time = strtotime(date('m',$h_date)."/1/".date('Y', $h_date));
-              $hours_worked[$lower_time] = (isset($hours_worked[$lower_time])) ? $hours_worked[$lower_time] : array();
+                //get the time for the index of array
+                $lower_time = (date('j', $h_date) > 15)  ? strtotime(date('m', $h_date)."/16/".date('Y', $h_date)) :
+                                $lower_time = strtotime(date('m', $h_date)."/1/".date('Y', $h_date));
+                $hours_worked[$lower_time] = (isset($hours_worked[$lower_time])) ? $hours_worked[$lower_time] : array();
               
-              //add employee hours for pay period
-              $hours_worked[$lower_time][$hour->employee_id] = (isset($hours_worked[$lower_time][$hour->employee_id])) ?
-                        ($hours_worked[$lower_time][$hour->employee_id] + $hour->hours_worked ) : $hour->hours_worked;
+                //add employee hours for pay period
+                $hours_worked[$lower_time][$hour->employee_id] = (isset($hours_worked[$lower_time][$hour->employee_id])) ?
+                        ($hours_worked[$lower_time][$hour->employee_id] + $hour->hours_worked) : $hour->hours_worked;
+            }
           
-          }
-          
-          //compute amount paid to each employee
-          foreach( $hours_worked as $index => $emp_entry ){
-                foreach( $emp_entry as $emp_id => $h_worked ){
-                  $emp = App\Employee::find($emp_id);
-                  $job = App\JobGroup::find(intval($emp->job_group_id));
-                  $hours_worked[$index][$emp_id] = number_format((float)($h_worked * $job->wage) , 2, '.', '' );
+            //compute amount paid to each employee
+            foreach ($hours_worked as $index => $emp_entry) {
+                foreach ($emp_entry as $emp_id => $h_worked) {
+                    $emp = App\Employee::find($emp_id);
+                    $job = App\JobGroup::find(intval($emp->job_group_id));
+                    $hours_worked[$index][$emp_id] = number_format((float)($h_worked * $job->wage), 2, '.', '');
                 }
-          }
+            }
           
-          //sort array by key
-          ksort($hours_worked);
-          $data['hours_worked'] = $hours_worked;
+            //sort array by key
+            ksort($hours_worked);
+            $data['hours_worked'] = $hours_worked;
           
-           //go to view with view data
-          return view('report.view', $data);
+            //go to view with view data
+            return view('report.view', $data);
         } else {
             return redirect('/')->with('warning', 'Invalid report selected.');
         }
@@ -123,7 +123,8 @@ class ReportController extends Controller
     /*
      * process and store data from report file
      */
-    private function process_report_file($file, $report_id) {
+    private function process_report_file($file, $report_id)
+    {
         
         //create report
         $report = App\Report::create(array('id'=>$report_id));
@@ -132,7 +133,7 @@ class ReportController extends Controller
         //auto detect line endings
         ini_set("auto_detect_line_endings", true);
         
-        $fp = fopen($file,'r');
+        $fp = fopen($file, 'r');
         
         //header columns
         $date = 0;
@@ -140,20 +141,20 @@ class ReportController extends Controller
         $emp_id = 2;
         $job_group = 3;
         
-        while( ($line = fgetcsv($fp, 0, ',')) !== FALSE){
-            if( $this->format_text($line[$date]) != 'date' && $this->format_text($line[$date]) !== 'report id') {
-              //make sure job group exists
-                $job = App\JobGroup::where('name','=',trim($line[$job_group]))->first();
-                if( $job->count() == 0) {
+        while (($line = fgetcsv($fp, 0, ',')) !== false) {
+            if ($this->format_text($line[$date]) != 'date' && $this->format_text($line[$date]) !== 'report id') {
+                //make sure job group exists
+                $job = App\JobGroup::where('name', '=', trim($line[$job_group]))->first();
+                if ($job->count() == 0) {
                     //don't enter data for job that doesn't exists in database
                     continue;
                 }
                 //see if employee exists if not create one
-                if( is_null( $employee = App\Employee::find(intval($line[$emp_id]))) ){
-                  $employee  = new App\Employee();
-                  $employee->id = intval($line[$emp_id]);
-                  $employee->job_group_id = $job->id;
-                  $employee->save();
+                if (is_null($employee = App\Employee::find(intval($line[$emp_id])))) {
+                    $employee  = new App\Employee();
+                    $employee->id = intval($line[$emp_id]);
+                    $employee->job_group_id = $job->id;
+                    $employee->save();
                 }
                 
                 //add hours for employee
@@ -161,40 +162,41 @@ class ReportController extends Controller
                 $hours = new App\Hour();
                 $hours->report_id = $report->id;
                 $hours->hours_worked = $line[$hours_worked];
-                $hours->date = date('Y-m-d H:i:s', strtotime( str_replace('/','-',$line[$date])));
+                $hours->date = date('Y-m-d H:i:s', strtotime(str_replace('/', '-', $line[$date])));
                 $hours->employee_id = $employee->id;
                 $hours->save();
             }
         }
-        
     }
     /*
      * helper function for formatiing text for comparisons
      */
-    private function format_text($text){
-      return strtolower(trim($text));
+    private function format_text($text)
+    {
+        return strtolower(trim($text));
     }
     /*
      * get a report number from a file
-     * @input $file - path 
-     * 
+     * @input $file - path
+     *
      * @return $report_id or FALSE
      */
-    private function get_report_number_from_file($file) {
+    private function get_report_number_from_file($file)
+    {
         
         //auto detect line endings
         ini_set("auto_detect_line_endings", true);
         
         $fp = fopen($file, 'r');
         
-        $report_id = FALSE;
+        $report_id = false;
         
         //find report number
-        while( ($line = fgetcsv($fp, 0, ',')) !== FALSE){
-          if( trim(strtolower($line[0])) == 'report id') {
+        while (($line = fgetcsv($fp, 0, ',')) !== false) {
+            if (trim(strtolower($line[0])) == 'report id') {
                 $report_id = $line[1];
                 break;
-          }
+            }
         }
         fclose($fp);
         
--- resources/views/report/reports.blade.php
+++ resources/views/report/reports.blade.php
@@ -7,7 +7,7 @@
     <div class="col-md-5">
         <div id="upload_report" class="card">
             <div class="card-body">
-                <h1>Upload New Report</h1>
+                <h1 class="card-title">Upload New Report</h1>
                <form id='report_form' class='form-horizontal' method="POST" action="@php echo route('upload'); @endphp" enctype='multipart/form-data'>
                    {{ csrf_field() }}
                    <div class="form-group">
@@ -23,21 +23,25 @@
 <div class="row container-fluid justify-content-center mt-3">
     @if($reports->count() > 0 )
     <div class="col-md-5">
-        <h1>Reports</h1>
-        <table class='table'>
-            <thead>
-            <th>Report Id</th>
-            <th>Actions</th>
-            </thead>
-            <tbody>
-              @foreach($reports as $report)
-              <tr>
-                  <td>@php echo $report->id;@endphp</td>
-                  <td><a titl="view report" href='@php echo route("view_report",["id"=>$report->id]);@endphp'><i class="far fa-eye"></i></a></td>
-              </tr>
-              @endforeach
-            </tbody>
-        </table>
+        <div class="card">
+            <div class="card-body">
+              <h1 class="card-title">Reports</h1>
+              <table class='table'>
+                  <thead>
+                  <th>Report Id</th>
+                  <th>Actions</th>
+                  </thead>
+                  <tbody>
+                    @foreach($reports as $report)
+                    <tr>
+                        <td>@php echo $report->id;@endphp</td>
+                        <td><a titl="view report" href='@php echo route("view_report",["id"=>$report->id]);@endphp'><i class="far fa-eye"></i></a></td>
+                    </tr>
+                    @endforeach
+                  </tbody>
+              </table>
+            </div>
+        </div>
     </div>
     @else
         <div class="col-md-8">
